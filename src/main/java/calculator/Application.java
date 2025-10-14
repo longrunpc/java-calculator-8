86215def2b24e7fca8e380a277e62949adb775be
longrunpc <pcjmo4050@gmail.com>
2025-10-14 23:31:15 +0900

    feat: CalculatorService interface 추가
    Calculator 서비스 인터페이스인 CalculatorService 파일 생성
    문자열을 입력받아 계산 결과를 문자열로 반환하는 메서드 calculateSum 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,12 @@
 package calculator;
 
+import calculator.controller.CalculatorController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // 의존성 주입
+        
+        // 계산 로직 실행
+        calculatorController.calculateSumByString();
     }
 }
