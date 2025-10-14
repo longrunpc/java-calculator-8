212fd8b0782052226350520c0c18d71f7a5ae165
longrunpc <pcjmo4050@gmail.com>
2025-10-14 17:44:40 +0900

    feat: 계산 결과 출력 기능 추가
    OutputView 클래스에 계산 결과를 출력하는 printResult 메서드를 추가하였습니다.

---
+++ src/main/java/calculator/view/OutputView.java
@@ -0,0 +1,13 @@
+package calculator.view;
+
+public class OutputView {
+    
+    /**
+     * 계산 결과를 출력하는 메서드
+     * 
+     * @param result
+     */
+    public static void printResult(String result) {
+        System.out.println("결과 : " + result);
+    }
+}
